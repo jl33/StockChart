1c0fec5c103b8beac6f8407012dea1ce4324815d
jason liao <jason.liao.2005@gmail.com>
2020-08-23 14:16:31 +0800

    test: yAxis.offset=containerD.clientWidth

--- getData.js
+++ getData.js
@@ -798,8 +798,9 @@ Highcharts.setOptions({
 //#region render chart...
 function mapChart() {
 	//#region base var
+	console.log(document.getElementById('containerD').clientWidth);
 	var _leftYLabTitlePos = -55,
-		_leftYLabOffset = 100 - document.getElementById('containerD').width; //-460;
+		_leftYLabOffset = 100 - document.getElementById('containerD').clientWidth; //-460;
 	//#endregion
 	//#region  chart~
 	_chart_D = Highcharts.stockChart('containerD', {
